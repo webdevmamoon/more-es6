fddb4b40822873fdeda2774a813cb7c4c5d5dbe6
abdullahalmamoon <abdullahalmamoon9@gmail.com>
2022-09-11 22:20:38 +0600

    added forEach js file

---
+++ forEach.js
@@ -0,0 +1,11 @@
+const brothers = ['Mizanur', 'Mamoon', 'Minan', 'Munna'];
+
+// brothers.forEach(brother => console.log(brother));
+
+const products = [
+    { id: 1, name: 'laptop', price: 45000 },
+    { id: 2, name: 'mobile', price: 80000 },
+    { id: 3, name: 'watch', price: 35000 },
+    { id: 4, name: 'tablet', price: 23000 },
+];
+products.forEach(product => console.log(product));
